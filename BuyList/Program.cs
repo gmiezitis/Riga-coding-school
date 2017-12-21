d896d74a8268c641884ca82270fb4df441748f12
gmiezitis <gabl@inbox.lv>
2017-12-21 21:15:51 +0200

    created smarter calculator

---
+++ BuyList/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BuyList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Ludzu ievadiet pirkumu!");
+
+        }
+    }
+}
---
+++ Smartcalculator/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Smartcalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            string operand;
+            float answer;
+
+            Console.WriteLine("Ievadi lūdzu pirmo numuru:\n");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ludzu izvēlaties operacijas (+, *, -, /):\n");
+            operand = Console.ReadLine();
+
+            Console.WriteLine("Ievadi lūdzu pirmo numuru:\n");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            switch (operand)
+            {
+                case "+":
+                    answer = num1 + num2;
+                    break;
+                case "*":
+                    answer = num1 * num2;
+                    break;
+
+                case "-":
+                    answer = num1 - num2;
+                    break;
+
+                case "/":
+                    answer = num1 / num2;
+                    break;
+                default:
+                    answer = 0;
+                    break;
+
+
+            }
+            Console.WriteLine("Atbilde ir :\n" + num1.ToString() + " " + operand + " " + num2.ToString() + " = " + answer.ToString());
+            Console.ReadLine();
+        }
+    }
+}
