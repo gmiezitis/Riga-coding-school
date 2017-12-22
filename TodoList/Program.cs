c7afab9d5352e2a8700da235299761ab6e670a96
gmiezitis <gabl@inbox.lv>
2017-12-22 21:07:41 +0200

    Todo list pievienots

---
+++ TodoList/Program.cs
@@ -0,0 +1,97 @@
+﻿
+
+using System;
+using TodoListFunctionality;
+using System.Collections.Generic;
+using System.IO;
+
+namespace TodoList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<TodoListManager> managers = new List<TodoListManager>();
+            
+
+            while (true)
+            {
+                Console.WriteLine("Please choose option:");
+                Console.WriteLine("1 - add new todo list");
+                Console.WriteLine("2 - remove todo list");
+                Console.WriteLine("3 - manipulate todo list");
+                Console.WriteLine("0 - quit");
+                int input = int.Parse(Console.ReadLine());
+
+
+                switch (input)
+                {
+                    case 1:
+                        Console.WriteLine("What will be the name?");
+                        string nameInput = Console.ReadLine();
+                        TodoListManager manager;
+                        manager = new TodoListManager();
+
+                        manager.NameOfToDoList = nameInput;
+                        
+                        managers.Add(manager);
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        for (int i = 0; i < managers.Count; i++)
+                        {
+                            Console.WriteLine(i + 1 + ". " + managers[i].NameOfToDoList);
+                        }
+                        int choiceInput = int.Parse(Console.ReadLine());
+                        TodoListManager selectedManager = managers[choiceInput - 1];
+                        ShowUserOptions(selectedManager);
+                        break;
+                    case 0:
+                        return;
+                }
+
+            }
+
+            Console.ReadKey();
+        }
+
+        public static void ShowUserOptions(TodoListManager manager)
+        {
+            while (true)
+            {
+                Console.WriteLine("please choose action");
+                Console.WriteLine("1 - add a task");
+                Console.WriteLine("2 - show added tasks");
+                Console.WriteLine("3 - delete a task");
+                Console.WriteLine("4 - save to file");
+                Console.WriteLine("q - quit");
+
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        Console.WriteLine("please enter task");
+                        string inputTask = Console.ReadLine();
+                        manager.AddNewTask(inputTask);
+                        break;
+                    case "2":
+                        manager.ShowTodos();
+                        break;
+                    case "3":
+                        manager.DeleteTodo();
+                        break;
+                    case "q":
+                        Console.WriteLine("you chose to exit");
+                        return;
+                        break;
+                    case "4":
+                        manager.SaveToFile();
+                        break;
+                }
+            }
+
+        }
+    }
+}
---
+++ newBuyList/EmptyClass.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+namespace newBuyList
+{
+    public class TodoListManager
+    {
+        public TodoListManager()
+        {
+
+        }
+
+        public TodoListManager(string path)
+        {
+            LoadList(path);
+        }
+
+        public string NameOfToDoList;
+
+        private List<string> todoTasks = new List<string>();
+        public void AddNewTask(string task)
+        {
+            todoTasks.Add(task);
+        }
+
+        public void DeleteTodo()
+        {
+            ShowTodos();
+            Console.WriteLine("enter number of todo to delte:");
+            int input = int.Parse(Console.ReadLine());
+            int indexInList = input - 1;
+            if (todoTasks.Count > indexInList)
+            {
+                todoTasks.RemoveAt(indexInList);
+            }
+        }
+
+        public void ShowTodos()
+        {
+            Console.WriteLine(NameOfToDoList + " has this to do: ");
+
+            for (int i = 0; i < todoTasks.Count; i++)
+            {
+                if (todoTasks[i] == "")
+                {
+                    Console.WriteLine("--[NO ENTRY HERE]--");
+                    continue;
+                }
+                Console.WriteLine((i + 1) + ". " + todoTasks[i]);
+            }
+
+        }
+
+        public void SaveToFile()
+        {
+            File.WriteAllLines("/Users/oscar/Desktop/riga coding school/so.txt ", this.todoTasks);
+         
+        }
+
+        public void LoadList()
+        {
+
+        }
+
+        public void LoadList(string path)
+        {
+
+        }
+    }
+}
