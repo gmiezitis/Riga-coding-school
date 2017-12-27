cb40d4e3c58bfc445f348764d399ae9e233bc77f
Gunars <gabl@inbox.lv>
2017-12-27 16:16:00 +0200

    New buy list

---
+++ BuyList/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BuyList
+{
+    using System.Collections.ObjectModel;
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<string> BuyItemsList = new ObservableCollection<string>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            // izveidojam mainigo
+                
+          BuyListItemName.Text = "Lūdzu ievadiet pirkumu";
+            this.BuyItemsList.Add("āboli");
+            this.BuyItemsList.Add("Bumbieri");
+       
+        //pasakām BuyItemListControl ka jaizmanto m'usu saraksts
+        // kā rādamo lietu avots (jāskatās no saraksta, ko rādīt) 
+        this.BuyItmesListControl.ItemsSource = this.BuyItemsList;
+           
+        }
+
+        private void AddListItemButton_Click(object sender, RoutedEventArgs e)
+        {
+            //izvelkam vertibu no teksta lauka 
+            string input = this.BuyListItemName.Text;
+            //nodzesam vertibu teksta lauka
+            this.BuyListItemName.Text = "";
+
+            //ierakstam ieguto vertibu teksta blo-ka
+            this.BuyItemsList.Add(input);
+        }
+    }
+}
