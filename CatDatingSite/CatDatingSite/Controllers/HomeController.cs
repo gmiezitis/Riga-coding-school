da8e54475d870ae7e927fbada2f62e250193dcf2
Gunars <gabl@inbox.lv>
2018-01-03 18:27:45 +0200

    removed unnecessary files

---
+++ CatDatingSite/CatDatingSite/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CatDatingSite.Controllers
+{
+    using CatDatingSite.Models;
+
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var catFromDB = new CatProfile();
+            return View(catFromDB);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ CatDatingSite/CatDatingSite/Models/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CatDatingSite.Models
+{
+    public class CatProfile
+    {
+       public string CatName;
+        public int CatAge;
+    }
+}
